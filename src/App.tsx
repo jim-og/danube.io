380012065752d83eb8a905327aaf90f4f017731c
Jim Gartland <jim@danube.io>
2026-04-16 15:25:57 +0000

    Update react-router-dom and typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/pages/Home';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -8,9 +8,9 @@ const App = () => {
   return (
     <Router>
       <div className="App">
-        <Switch>
-          <Route exact path="/" component={Home} />
-        </Switch>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </div>
     </Router>
   );
